699e62a450bbd6efabc8654910a77f6d7a0d6f12
sd_vivo2020 <sd_vivo2020@bbktel.com>
2022-08-09 09:29:46 +0800

    修复上传文件时会重新创建工程目录及files文件夹的问题

--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -18,8 +18,8 @@ public class FileController {
     @PostMapping("upload")
     public Result<?> upload(MultipartFile file) throws IOException {  /*MultipartFile file用于接收前台传过来的文件数据对象*/
         String originalFilename = file.getOriginalFilename();  //获取文件名称，需要思考怎么解决相同文件名重复的问题
-        //首先通过System.getProperty("user.dir")获取Springboot这个工程的路径地址，再拼接上files文件夹的路径及文件的路径，即是文件存储的绝对路径
-        String rootFilePath = System.getProperty("user.dir") + "/turtleServer/src/main/resources/files/" + originalFilename;
+        //首先通过System.getProperty("user.dir")获取Springboot这个工程的路径地址，再拼接上files文件夹的路径及文件的路径，即是文件存储的绝对路径，注意后面拼接的时候不需要再带工程名，前面已经获取到了
+        String rootFilePath = System.getProperty("user.dir") + "/src/main/resources/files/" + originalFilename;
         FileUtil.writeBytes(file.getBytes(), rootFilePath);  //使用Hutool工具类提供的工具FileUtil进行IO数据流的读写，直接将传过来的文件以字节流的形式file.getBytes()写入文件夹
         return Result.success();
     }
